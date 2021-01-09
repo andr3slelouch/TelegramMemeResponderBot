50f881b82c9431b7cebc39fa220dca8f009e52ac
andr3slelouch <luis.andradec14@gmail.com>
2021-01-09 17:48:45 -0500

    First release

---
+++ bot/bot.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=W0613, C0116
+# type: ignore[union-attr]
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+)
+from datetime import datetime, timezone
+import re
+from unicodedata import normalize
+import pandas as pd
+
+START_BOT_DATETIME = datetime.now(timezone.utc)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text("Hi!")
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text("Help!")
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    if update.message.date > START_BOT_DATETIME:
+        # update.message.reply_text(update.message.text)
+        if get_meme_url(string_normalizer(update.message.text)):
+            update.message.reply_photo(
+                get_meme_url(string_normalizer(update.message.text))
+            )
+
+
+def string_normalizer(phrase: str) -> str:
+    phrase = re.sub(
+        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+",
+        r"\1",
+        normalize("NFD", phrase),
+        0,
+        re.I,
+    )
+    # -> NFC
+    phrase = normalize("NFC", phrase)
+    phrase = phrase.replace(",", "")
+    phrase = phrase.replace("?", "")
+    phrase = phrase.replace("¿", "")
+    phrase = phrase.lower()
+    return phrase
+
+
+def get_meme_url(meme: str) -> str:
+    try:
+        df = pd.read_excel("data/meme_bot_db.xlsx")
+        meme_df = df.loc[df["Meme"] == meme]
+        return meme_df.iloc[0, 1]
+    except:
+        return False
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(
+        "1515669604:AAHH4PLj_dF6w01Ke16DJ30w04aCHEImxwk", use_context=True
+    )
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
