e3c5dbf8e9c9b5d7f0dd4c26b8540292016992ba
Andrés Andrade <45749416+andr3slelouch@users.noreply.github.com>
2021-03-03 16:33:07 -0500

    Update syncronize.sh

--- syncronize.sh
+++ syncronize.sh
@@ -1,2 +1,2 @@
 cp "/home/andres/Insync/luis.andradec14@gmail.com/Google Drive/Projects/MemeBotDatabase/meme_bot_db.xlsx" ./data/
-sshpass -p "YOUR PASSWORD" rsync -a bot data pi@10.42.0.1:/home/pi/Projects/memeBot
+sshpass -p "YOUR PASSWORD" rsync -a bot data pi@"YOUR SERVER IP":/home/pi/Projects/memeBot
