de8b75ad7a0a86ec5018883c134777211f2973d8
andr3slelouch <andr3slelouch@github.com>
2023-12-19 17:20:49 -0500

    Added llm 0.0.1

--- memebot/llm_chat_manager.py
+++ memebot/llm_chat_manager.py
@@ -1,5 +1,25 @@
+import logging
+
 from llama_cpp import Llama
 from config.config import load_config
+from cysystemd.journal import JournaldLogHandler
+
+# get an instance of the logger object this module will use
+logger = logging.getLogger(__name__)
+
+# instantiate the JournaldLogHandler to hook into systemd
+journald_handler = JournaldLogHandler()
+
+# set a formatter to include the level name
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+
+# add the journald handler to the current logger
+logger.addHandler(journald_handler)
+
+# optionally set the logging level
+logger.setLevel(logging.DEBUG)
 
 
 class LlmChatManager:
@@ -16,5 +36,7 @@ class LlmChatManager:
 
         if prompt and "{MESSAGE}" in prompt:
             prompt = prompt.replace("{MESSAGE}", message)
-            output = self.llm(prompt, self.max_tokens, self.stop)
+            logger.info(f"Prompt: {prompt}")
+            logger.info(f"max_tokens: {self.max_tokens}, stop: {self.stop}")
+            output = self.llm(prompt, max_tokens=self.max_tokens, stop=self.stop)
             return output.get("choices", [{}])[0].get("text", "")
